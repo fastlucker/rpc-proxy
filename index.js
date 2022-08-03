9483e612410d0f8a333242ff92ba7616b496facb
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2022-08-03 16:32:53 +0300

    Add basic logic for pickup next provider on round-robin

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(123)
+let counter = 0
+
+const providers = [
+    'rpc1',
+    'rpc2',
+]
+
+function getNextProviderIndex () {
+    return counter % providers.length
+}
+
+function getProvider () {
+    const nextProviderIndex = getNextProviderIndex()
+
+    console.log(`provider counter: ${counter}`)
+    console.log(`provider index: ${nextProviderIndex}`)
+    console.log(`provider url: ${providers[nextProviderIndex]}`)
+
+    counter++
+}
+
+module.exports = { getProvider }
