7c9ec297d2313020df40e11640d9f2f1cde47069
Borislav Itskov <borislav@devlabs.bg>
2022-08-04 13:08:24 +0300

    encapsulate the logic so a provider is getting returned to the relayer but with the option to extend it's methods

--- index.js
+++ index.js
@@ -1,60 +1,101 @@
 const { StaticJsonRpcProvider, WebSocketProvider } = require('ethers').providers
 
-let counter = 0
-const byNetwork = {}
-
 const providers = {
-    polygon: [
-        'https://polygon-rpc.com/rpc',
-        'https://rpc.ankr.com/polygon',
-    ]
+  polygon: [
+    'https://polygon-rpc.com/rpc',
+    'https://rpc.ankr.com/polygon',
+  ]
 }
 
-function getCurrentProviderIndex (network) {
-    return counter % providers[network].length
-}
+class CustomRPC {
+  counter = 0;
+  byNetwork = {}
+  provider = null
 
-function getProviderUrl (network) {
-    const nextProviderIndex = getCurrentProviderIndex(network)
+  constructor(networkName, chainId) {
+    if (networkName !== 'polygon') return
 
-    console.log(`provider counter: ${counter}`)
+    // move the counter
+    this.counter++
+    const currentIndex = this.getCurrentProviderIndex(networkName);
+    if (this.byNetwork[networkName] && this.byNetwork[networkName][currentIndex]) {
+      return this.byNetwork[networkName][currentIndex]
+    }
+
+    const url = this.getProviderUrl(networkName)
+    this.provider = url.startsWith('wss:')
+      ? new WebSocketProvider(url, { networkName, chainId })
+      : new StaticJsonRpcProvider(url, { networkName, chainId })
+
+    if (this.provider) {
+      this.provider.on('error', function (e) {
+        console.error(`[${new Date().toLocaleString()}] RPC "[${url}]" return error`, e)
+      })
+    }
+
+    if (this.provider && this.provider._websocket && this.provider._websocket.on) {
+      this.provider._websocket.on('error', function (e) {
+        console.error(`[${new Date().toLocaleString()}] provider RPC "[${url}]" return socket error`, e)
+      })
+    }
+
+    if (! this.byNetwork[networkName]) this.byNetwork[networkName] = {}
+    this.byNetwork[networkName][currentIndex] = this.provider;
+  }
+
+  getCurrentProviderIndex (network) {
+    return this.counter % providers[network].length
+  }
+
+  getProviderUrl (network) {
+    const nextProviderIndex = this.getCurrentProviderIndex(network)
+
+    console.log(`provider counter: ${this.counter}`)
     console.log(`provider index: ${nextProviderIndex}`)
     console.log(`provider url: ${providers[network][nextProviderIndex]}`)
 
     return providers[network][nextProviderIndex]
+  }
+
+  getProvider() {
+    return this.provider
+  }
+
+  getBlockWithTransactions(blockHashOrBlockTag) {
+    console.log('I am extending getBlockWithTransactions')
+    return this.provider.getBlockWithTransactions(blockHashOrBlockTag)
+  }
+
+  // send(method, params) {
+  //   console.log('I am extending send')
+  //   return this.provider.send(method, params)
+  // }
 }
 
-function getProvider (networkName, chainId) {
-    if (networkName !== 'polygon') return null;
+function getProvider(networkName, chainId) {
+  const rpc = new CustomRPC(networkName, chainId)
+  if (rpc.getProvider() == null) return null;
 
-    // move the counter
-    counter++
-    const currentIndex = getCurrentProviderIndex(networkName);
-    if (byNetwork[networkName] && byNetwork[networkName][currentIndex]) {
-        console.log(currentIndex)
-        return byNetwork[networkName][currentIndex]
+  return new Proxy(rpc, {
+    get: function get(target, name) {
+      return function wrapper() {
+
+        // if we want to extend/override explicity the method,
+        // we do so here
+        if (typeof(rpc[name]) == 'function') {
+          return rpc[name](...arguments)
+        }
+
+        // if we want just want to proceed with the normal exec,
+        // we pass it on to the original provider
+        const provider = rpc.getProvider()
+        if (typeof(provider[name]) == 'function') {
+          console.log(name)
+          return provider[name](...arguments)
+        }
+      }
     }
-
-    const url = getProviderUrl(networkName)
-	const provider = url.startsWith('wss:')
-        ? new WebSocketProvider(url, { networkName, chainId })
-        : new StaticJsonRpcProvider(url, { networkName, chainId })
-
-	if (provider) {
-		provider.on('error', function (e) {
-			console.error(`[${new Date().toLocaleString()}] RPC "[${url}]" return error`, e)
-		})
-	}
-
-	if (provider && provider._websocket && provider._websocket.on) {
-		provider._websocket.on('error', function (e) {
-			console.error(`[${new Date().toLocaleString()}] provider RPC "[${url}]" return socket error`, e)
-		})
-	}
-
-    if (! byNetwork[networkName]) byNetwork[networkName] = {}
-    byNetwork[networkName][currentIndex] = provider;
-	return provider
+  });
 }
 
 module.exports = { getProvider }
