195a1be71fbaafa627a58abe1b2f8a17407311d3
Borislav Itskov <borislav@devlabs.bg>
2022-08-04 06:50:35 +0300

    add an ethers dependancy; add a method for retrieving the provider directly

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { StaticJsonRpcProvider, WebSocketProvider } = require('ethers').providers
+
 let counter = 0
 
 const providers = {
@@ -22,4 +24,27 @@ function getProviderUrl (network) {
     return providers[network][nextProviderIndex]
 }
 
-module.exports = { getProviderUrl }
+function getProvider (networkName, chainId) {
+    if (networkName !== 'polygon') return null;
+
+    const url = getProviderUrl(networkName)
+	const provider = url.startsWith('wss:')
+        ? new WebSocketProvider(url, { networkName, chainId })
+        : new StaticJsonRpcProvider(url, { networkName, chainId })
+
+	if (provider) {
+		provider.on('error', function (e) {
+			console.error(`[${new Date().toLocaleString()}] RPC "[${url}]" return error`, e)
+		})
+	}
+
+	if (provider && provider._websocket && provider._websocket.on) {
+		provider._websocket.on('error', function (e) {
+			console.error(`[${new Date().toLocaleString()}] provider RPC "[${url}]" return socket error`, e)
+		})
+	}
+
+	return provider
+}
+
+module.exports = { getProvider }
