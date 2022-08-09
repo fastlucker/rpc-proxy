456c445667d2de457bd45e52efc79da7c2a29216
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2022-08-09 20:40:32 +0300

    Move providers to a separate file

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 const { StaticJsonRpcProvider, WebSocketProvider } = require('ethers').providers
 
-const providers = {
-  ethereum: {
-    RPCs: [
-      {url: 'https://eth-mainnet.alchemyapi.io/v2/e5Gr8LP_EH0SBPZiNCcC08OuEDrvgoYK', tags: []},
-      {url: 'https://mainnet.infura.io/v3/d4319c39c4df452286d8bf6d10de28ae', tags: ['getLogs','eth_getLogs']},
-      {url: 'wss://mainnet.infura.io/ws/v3/d4319c39c4df452286d8bf6d10de28ae', tags: ['getLogs','eth_getLogs']}  
-    ],
-    chainId: 1
-  },
-  polygon: {
-    RPCs: [
-      {url: 'https://polygon-rpc.com/rpc', tags: ['call','eth_sendRawTransaction','getLogs','eth_getLogs']},
-      {url: 'https://rpc.ankr.com/polygon', tags: ['call']}
-    ],
-    chainId: 137
-  }
-}
+const providers = require('./providers')
 
 const byNetwork = {}
 const byNetworkCounter = {}
